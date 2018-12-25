72edf1c73aa55a911d7867d2d8e71dda88c1da4d
Romain <rmtestard@gmail.com>
2018-12-25 11:24:08 +0100

    Add Navigation Bar and Style App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
       <>
         {/*<React.Fragment />*/}
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
+        <Nav />
+        <div className="body">
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </>
     );
   }
