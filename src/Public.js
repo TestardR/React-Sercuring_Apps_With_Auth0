38db99dd7d9c048f61f9a3d5d99d325907c44c33
Romain <rmtestard@gmail.com>
2018-12-27 09:52:27 +0100

    Call Public API via React

---
+++ src/Public.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Public extends Component {
+  state = {
+    message: ''
+  };
+
+  componentDidMount() {
+    fetch('/public')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok !');
+        }
+      })
+      .then(response =>
+        this.setState({
+          message: response.message
+        })
+      )
+      .catch(error =>
+        this.setState({
+          message: error.message
+        })
+      );
+  }
+  render() {
+    return <p>{this.state.message}</p>;
+  }
+}
+
+export default Public;
