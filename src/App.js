bc7a0ded9fa7123eba7b073801c48cac1fc7d34b
Romain <rmtestard@gmail.com>
2018-12-28 10:24:53 +0100

    Declare AuthContext Provider

--- src/App.js
+++ src/App.js
@@ -9,41 +9,47 @@ import Public from './Public';
 import Private from './Private';
 import Courses from './Courses';
 import PrivateRoute from './PrivateRoute';
+import AuthContext from './AuthContext';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.auth = new Auth(this.props.history); // as index.js is wrapped, the history object is available to react-router. We pass history so that Auth can interact with react-router
+    this.state = {
+      auth: new Auth(this.props.history) // as index.js is wrapped, the history object is available to react-router. We pass history so that Auth can interact with react-router
+    };
   }
+
   render() {
+    const { auth } = this.state;
     return (
-      <>
+      // Child components can access the auth object by importing AuthContext.Consumer
+      <AuthContext.Provider value={auth}>
         {/*<React.Fragment />*/}
-        <Nav auth={this.auth} />
+        <Nav auth={auth} />
         <div className="body">
           <Route
             path="/"
             exact
-            render={props => <Home auth={this.auth} {...props} />}
+            render={props => <Home auth={auth} {...props} />}
           />
           <Route
             path="/callback"
-            render={props => <Callback auth={this.auth} {...props} />}
+            render={props => <Callback auth={auth} {...props} />}
           />
           {/*We pass the auth object down to each component on props for now.*/}
-          <PrivateRoute path="/profile" component={Profile} auth={this.auth} />
+          <PrivateRoute path="/profile" component={Profile} auth={auth} />
           <Route path="/public" component={Public} />
-          <PrivateRoute path="/private" component={Private} auth={this.auth} />
+          <PrivateRoute path="/private" component={Private} auth={auth} />
           <PrivateRoute
             path="/courses"
             component={Courses}
             // these checks are merely for user experience, not security.
             // It's the server's job to validate the user is authorized when an API call is made
-            auth={this.auth}
+            auth={auth}
             scopes={['read:courses']}
           />
         </div>
-      </>
+      </AuthContext.Provider>
     );
   }
 }
